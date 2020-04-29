6164efe27fcddbc82662275b3d066191dd911aff
ch3ng <ch3ng57@gmail.com>
2020-04-29 15:35:34 +0800

    add business server

---
+++ rainbow-biz/src/main/java/com/milchstrabe/rainbow/biz/RainbowBizApplication.java
@@ -0,0 +1,13 @@
+package com.milchstrabe.rainbow.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainbowBizApplication.class, args);
+    }
+
+}
