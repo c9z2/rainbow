eed0ec72eba54ab939efc8e810dfe9779fe38fc1
ch3ng <ch3ng57@gmail.com>
2020-05-17 12:39:23 +0800

    add websocket support

---
+++ rainbow-ws/src/main/java/com/milchstrabe/rainbow/ws/RainbowWsApplication.java
@@ -0,0 +1,13 @@
+package com.milchstrabe.rainbow.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowWsApplication.class, args);
+	}
+
+}
---
+++ rainbow-ws/src/test/java/com/milchstrabe/rainbow/ws/RainbowWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milchstrabe.rainbow.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
