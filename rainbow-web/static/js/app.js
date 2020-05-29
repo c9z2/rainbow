d557e128c4508ae3cbd9b1442fc18f82b48f61a9
ch3ng <ch3ng57@gmail.com>
2020-05-29 22:17:57 +0800

    add websocket clients test program

---
+++ rainbow-web/static/js/app.js
@@ -0,0 +1,94 @@
+
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/ws/rainbow-ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        var userId = localStorage.getItem("userId");
+        var sub = '/user/'+userId+'/message';
+        console.log(sub);
+        stompClient.subscribe(sub, function (greeting) {
+            console.log(greeting);
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/message", {}, JSON.stringify({'name': $("#name").val()}));
+    // $.get("/ws/send2",function(data){
+    //     console.log(data);
+    // })
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+function uuid() {
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+ 
+    var uuid = s.join("");
+    return uuid;
+}
+
+
+
+$(function () {
+  
+    //sign in web socket
+    var token  = localStorage.getItem("token");
+    $.post("http://localhost:8080/ws/sys/signIn",
+        {
+            "token":token,
+            "cid":uuid(),
+            "clientType":"WEB"
+        },
+        function(data){
+            console.log(data);
+
+            
+        });
+
+
+      $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+
+    
+});
+
+
